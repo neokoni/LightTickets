5b127756fdb1b175248720fc3af6796f593bfe30
neokoni <dulin081682@126.com>
2026-06-30 14:23:59 +0800

    plugin: init project
    Signed-off-by: neokoni <dulin081682@126.com>

---
+++ plugin/src/main/java/ink/neokoni/lightTickets/LightTickets.java
@@ -0,0 +1,16 @@
+package ink.neokoni.lightTickets;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LightTickets extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ plugin/src/main/java/ink/neokoni/lightTickets/LightTicketsLoader.java
@@ -0,0 +1,12 @@
+package ink.neokoni.lightTickets;
+
+import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
+import io.papermc.paper.plugin.loader.PluginLoader;
+
+class LightTicketsLoader implements PluginLoader {
+
+    @Override
+    public void classloader(final PluginClasspathBuilder builder) {
+        // Add dynamically loaded libraries here
+    }
+}
